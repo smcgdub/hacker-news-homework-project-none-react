a2d6d754d669c7aefa1e2c3dcc72cf73cee1dcd9
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-09-22 13:26:16 +0100

    Added the getTopPosts JS function and logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Get new posts function
 function getNewPosts() {
   document.getElementById('output').innerHTML = ``;
   fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
@@ -20,6 +21,38 @@ function getNewPosts() {
               <p class="off-white-text mb-0">${story.type} by: ${story.by}</p>
               <p class="off-white-text mb-0">posted: ${humanDateFormat}</p>
               <p class="off-white-text mb-0">link to article:<a class="post-url" href="${story.url}" target="_blank"> Click here</a></p>
+              </div>
+          </div>
+          `
+          })
+        )
+      })
+    })
+}
+
+// Get top posts function
+function getTopPosts() {
+  document.getElementById('output').innerHTML = ``;
+  fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
+    .then(response => response.json())
+    .then(storyIds => {
+      storyIds.map(function (id) {
+        return (
+          fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+          .then(response => response.json())
+          .then(story => {
+            // Display time human readable
+            let unixTimestamp = story.time;
+            let milliseconds = unixTimestamp * 1000;
+            let dateObject = new Date(milliseconds);
+            let humanDateFormat = dateObject.toLocaleString();
+            document.getElementById('output').innerHTML += `
+          <div class="card mb-3">
+            <h5 id="post-title" class="card-header data-title mb-1">${story.title}</h5>
+            <div class="card-body">
+              <p class="mb-0 off-white-text">${story.type} by: ${story.by}</p>
+              <p class="mb-0 off-white-text">Posted: ${humanDateFormat}</p>
+              <p class="mb-0 off-white-text">Link to article:<a class="post-url" href="${story.url}" target="_blank"> Click here</a></p>
               </div>
           </div>
           `
